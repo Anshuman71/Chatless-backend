6e6825e2cf1586504e63bdbe7c227ed99f8e5795
Anshuman <anshu5074@gmail.com>
2018-11-28 15:01:09 +0530

    launch

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import http from 'http';
+import Socket from 'socket.io';
+
+const app = express();
+
+let connections = [];
+const server = http.createServer(app);
+
+const io = Socket.listen(server);
+
+app.get('/', (req, res) => {
+  res.send('<h1>Started now</h1>');
+});
+server.listen('2017' || process.env.PORT, () => console.log('Running on 2017'));
+
+io.sockets.on('connection', sock => {
+  connections.push(sock);
+  sock.on('disconnect', data => {
+    connections.splice(connections.indexOf(sock), 1);
+    console.log('disconnected ', { data });
+  });
+
+  sock.on('sendMessage', data => {
+    const id = sock.id;
+    console.log({ data }, 'New message');
+    io.sockets.emit('newMessage', { message: { data, id } });
+  });
+  sock.on('isTyping', data => {
+    console.log(data);
+    io.sockets.emit('typing', { typing: data });
+  });
+});
