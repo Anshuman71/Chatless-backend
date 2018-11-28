e3696e0a91cc6afc735c774a872eaedde11cf851
Anshuman <anshu5074@gmail.com>
2018-11-28 17:27:31 +0530

    run

--- src/server.js
+++ src/server.js
@@ -13,20 +13,20 @@ app.get('/', (req, res) => {
 });
 server.listen('5000' || process.env.PORT, () => console.log('Running on 2017'));
 
-io.sockets.on('connection', sock => {
-  connections.push(sock);
-  sock.on('disconnect', data => {
-    connections.splice(connections.indexOf(sock), 1);
-    console.log('disconnected ', { data });
-  });
+// io.sockets.on('connection', sock => {
+//   connections.push(sock);
+//   sock.on('disconnect', data => {
+//     connections.splice(connections.indexOf(sock), 1);
+//     console.log('disconnected ', { data });
+//   });
 
-  sock.on('sendMessage', data => {
-    const id = sock.id;
-    console.log({ data }, 'New message');
-    io.sockets.emit('newMessage', { message: { data, id } });
-  });
-  sock.on('isTyping', data => {
-    console.log(data);
-    io.sockets.emit('typing', { typing: data });
-  });
-});
+//   sock.on('sendMessage', data => {
+//     const id = sock.id;
+//     console.log({ data }, 'New message');
+//     io.sockets.emit('newMessage', { message: { data, id } });
+//   });
+//   sock.on('isTyping', data => {
+//     console.log(data);
+//     io.sockets.emit('typing', { typing: data });
+//   });
+// });
