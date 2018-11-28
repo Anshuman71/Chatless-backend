a7c02e58e507066b2d3d44dfa2ff02a301413987
Anshuman <anshu5074@gmail.com>
2018-11-28 17:34:26 +0530

    should run

--- src/server.js
+++ src/server.js
@@ -12,22 +12,22 @@ app.get('/', (req, res) => {
   res.send('<h1>Started now</h1>');
 });
 
-app.listen('5000' || process.env.PORT, () => console.log('Running on 2017'));
+server.listen(process.env.PORT || 5000, () => console.log('Running on 2017'));
 
-// io.sockets.on('connection', sock => {
-//   connections.push(sock);
-//   sock.on('disconnect', data => {
-//     connections.splice(connections.indexOf(sock), 1);
-//     console.log('disconnected ', { data });
-//   });
+io.sockets.on('connection', sock => {
+  connections.push(sock);
+  sock.on('disconnect', data => {
+    connections.splice(connections.indexOf(sock), 1);
+    console.log('disconnected ', { data });
+  });
 
-//   sock.on('sendMessage', data => {
-//     const id = sock.id;
-//     console.log({ data }, 'New message');
-//     io.sockets.emit('newMessage', { message: { data, id } });
-//   });
-//   sock.on('isTyping', data => {
-//     console.log(data);
-//     io.sockets.emit('typing', { typing: data });
-//   });
-// });
+  sock.on('sendMessage', data => {
+    const id = sock.id;
+    console.log({ data }, 'New message');
+    io.sockets.emit('newMessage', { message: { data, id } });
+  });
+  sock.on('isTyping', data => {
+    console.log(data);
+    io.sockets.emit('typing', { typing: data });
+  });
+});
