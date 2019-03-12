6bc6ae505abdd092f55e04a2de0f1542b79bccb7
Anshuman <anshu5074@gmail.com>
2019-03-12 20:25:46 +0530

    make http server for socket connection

--- src/server.js
+++ src/server.js
@@ -5,15 +5,20 @@ import Socket from 'socket.io';
 const app = express();
 
 let connections = [];
+
 const server = http.createServer(app);
 
 const io = Socket.listen(server);
+
+//confirm the app is running
 app.get('/', (req, res) => {
   res.send('<h1>Started now</h1>');
 });
 
+//listen
 server.listen(process.env.PORT || 5000, () => console.log('Running on 2017'));
 
+//capture events
 io.sockets.on('connection', sock => {
   connections.push(sock);
   sock.on('disconnect', data => {
@@ -21,11 +26,14 @@ io.sockets.on('connection', sock => {
     console.log('disconnected ', { data });
   });
 
+  //send message event
   sock.on('sendMessage', data => {
     const id = sock.id;
     console.log({ data }, 'New message');
     io.sockets.emit('newMessage', { message: { data, id } });
   });
+
+  //send typing event
   sock.on('isTyping', data => {
     console.log(data);
     io.sockets.emit('typing', { typing: data });
